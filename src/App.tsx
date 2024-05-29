371dde8b20ad1976e5351a64edc7b03f0650e22c
jjeong <zzhenxxi@gamil.com>
2024-05-29 12:57:46 +0900

    카운트증가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const count =0;
+  const 리턴값 = useState(0);
+  const count = 리턴값[0];
+  const setCount = 리턴값[1];
   return(
   <div>
     <h1>{count}</h1>
-    <button onClick={()=>count+1}>증가</button>
+    <button onClick={()=>setCount((현재카운트)=>현재카운트+1)}>증가</button>
   </div>
   );
 }
